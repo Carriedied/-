0a694b8f9328dd17e05bd664cb14f0c5d1069b95
Carriedied <52948394+Carriedied@users.noreply.github.com>
2020-05-12 15:25:32 +0700

    Add files via upload

---
+++ Кортежи/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Кортежи
+{
+    class Class1
+    {
+        public string[] ShowCities(Tuple<string, int, string>[] city)
+        {
+            string[] arr = new string[5];
+            string s = "";
+            int n = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if(i == 0)
+                {
+                    arr[n] = city[i].Item1;
+                    s = city[i].Item1;
+                    n++;
+                }
+                else
+                {
+                    if (s != city[i].Item1)
+                    {
+                        arr[n] = city[i].Item1;
+                        s = city[i].Item1;
+                        n++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            return arr;
+        }
+        public void Minimal(Tuple<string, int, string>[] city)            
+        {
+            int minimal = 100;
+            string s1 = null;
+            string s2 = null;
+            for (int i = 0; i < 12; i++)
+            {
+                if (city[i].Item2 < minimal)
+                {
+                    minimal = city[i].Item2;
+                    s1 = city[i].Item1;
+                    s2 = city[i].Item3;
+                }
+            }
+            Console.WriteLine("Самые близкие города: " + s1 + " и " + s2);
+        }
+    }
+}
---
+++ Кортежи/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Кортежи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuple<string, int, string>[] tuples =
+            {
+                Tuple.Create("A",5,"B"),
+                Tuple.Create("A",9,"D"),
+                Tuple.Create("B",5,"A"),
+                Tuple.Create("B",9,"E"),
+                Tuple.Create("C",7,"D"),
+                Tuple.Create("C",8,"E"),
+                Tuple.Create("D",9,"A"),
+                Tuple.Create("D",7,"C"),
+                Tuple.Create("D",6,"E"),
+                Tuple.Create("E",9,"B"),
+                Tuple.Create("E",8,"C"),
+                Tuple.Create("E",6,"D")
+            };
+            Console.WriteLine("Введите номер задания");
+            int n = 0;
+            while (n != 5)
+            {
+                Console.WriteLine(
+                    "1. Все города системы\n" +
+                    "2. Самые близкие города, между которыми есть связь хотя бы в одну сторону\n" +
+                    "3. Самые далекие города, между которыми есть связь хотя бы в одну сторону\n" +
+                    "4. Сумма всех направлений\n" +
+                    "5. Выход");
+                string num = Console.ReadLine();
+                if (String.IsNullOrEmpty(num))
+                {
+                    Console.WriteLine("Введите номер задания еще раз");
+                }
+                else
+                {
+                    n = Convert.ToInt32(num);
+                    Class1 cl = new Class1();
+                    switch (n)
+                    {
+                        case 1:
+                            for (int i = 0; i < 5; i++)
+                            {
+                                string[] arr = cl.ShowCities(tuples);
+                                Console.WriteLine(arr[i]);
+                            }
+                            break;
+                        case 2:
+                            cl.Minimal(tuples);
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
